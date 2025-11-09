2e21252a7c74b60ca312ad5e0b981c4031cdefa8
Akira Hayashi <rindrics@gmail.com>
2025-11-09 10:50:13 +0900

    Update default config path

--- main.go
+++ main.go
@@ -7,10 +7,12 @@ import (
 	"os"
 )
 
+const defaultConfigFile = ".gh-issue-config-filter.yml"
+
 func main() {
 	var (
 		month      = flag.Int("month", 0, "Month (1-12) to filter issues")
-		configFile = flag.String("config", "config.yaml", "Path to config file")
+		configFile = flag.String("config", "", "Path to config file")
 		debug      = flag.Bool("debug", false, "Enable debug logging")
 	)
 	flag.Parse()
@@ -22,8 +24,16 @@ func main() {
 		log.Fatalf("failed to parse month: %v", err)
 	}
 
-	config, err := LoadConfig(*configFile)
+	configPath := *configFile
+	if configPath == "" {
+		configPath = defaultConfigFile
+	}
+
+	config, err := LoadConfig(configPath)
 	if err != nil {
+		if os.IsNotExist(err) {
+			log.Fatalf("config file not found: %s\nUse --config to specify a different config file", configPath)
+		}
 		log.Fatalf("failed to load config: %v", err)
 	}
 
