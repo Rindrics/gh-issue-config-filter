9c8538038bc565f2a30303b488fb71d28aaaa142
Akira Hayashi <rindrics@gmail.com>
2025-11-09 10:05:29 +0900

    Implement entrypoint

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"log"
+	"os"
+)
+
+func main() {
+	var (
+		month      = flag.Int("month", 0, "Month (1-12) to filter issues")
+		output     = flag.String("output", "json", "Output format (json)")
+		configFile = flag.String("config", "config.yaml", "Path to config file")
+		debug      = flag.Bool("debug", false, "Enable debug logging")
+	)
+	flag.Parse()
+
+	SetDebugMode(*debug)
+
+	monthEnum, err := ParseMonth(*month)
+	if err != nil {
+		log.Fatalf("failed to parse month: %v", err)
+	}
+
+	config, err := LoadConfig(*configFile)
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
+	if err := ValidateConfig(config); err != nil {
+		log.Fatalf("config validation failed: %v", err)
+	}
+
+	issuesToCreate := GetIssuesToCreate(config, monthEnum)
+
+	switch *output {
+	case "json":
+		if err := outputJSON(issuesToCreate); err != nil {
+			log.Fatalf("failed to output JSON: %v", err)
+		}
+	default:
+		log.Fatalf("unsupported output format: %s", *output)
+	}
+}
+
+func outputJSON(issuesToCreate IssuesToCreate) error {
+	output := make([]map[string]interface{}, 0, len(issuesToCreate.Issues))
+	for _, issue := range issuesToCreate.Issues {
+		item := map[string]interface{}{
+			"name":          issue.Name,
+			"template_file": issue.TemplateFile,
+			"fields":        issue.Fields,
+			"project_id":    issue.ProjectID,
+			"target_repo":   issue.TargetRepo,
+		}
+		if issue.TitleSuffix != nil {
+			item["title_suffix"] = issue.TitleSuffix
+		}
+		output = append(output, item)
+	}
+
+	encoder := json.NewEncoder(os.Stdout)
+	encoder.SetIndent("", "  ")
+	return encoder.Encode(output)
+}
